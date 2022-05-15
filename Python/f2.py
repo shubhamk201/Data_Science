6ad8c3c69e60c26c15963386c519030cf77c172d
shubhamk201 <sk201.nitj@gmail.com>
2022-05-15 23:43:43 +0530

    My First commit and push to cloud Repo

---
+++ Python/f2.py
@@ -0,0 +1,13 @@
+By Nigel Cheshire
+Back when Lotus Notes was still wearing short pants, let's say before about 1995 or 19
+96, the issue of version control would never have come up in conversation around theN
+otes development team's water cooler. In fact, back then, most Notes developers probab
+ly wouldn't have even known what version control, revision control, source code contro
+ 1or whatever you want to call it, even was.
+Many people who found themselves building and maintaining Lotus Notes applications had
+ not come fromatraditional software engineering background, having started out asa
+“power user" and developed their skills in sync with the evolution of Notes as an appl
+ication platform. Admittedly,Iwent to school for computer sciencealong time ago (c
+lass of 19841) but even back then, it was drilled into me to use Sccs and Make for ver
+sion control and build management.
+
